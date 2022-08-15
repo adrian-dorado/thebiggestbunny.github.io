6977832928ba2d755531171df3cc218be7e38d28
rbbitphoto <rbbitphoto@gmail.com>
2022-08-14 18:38:38 -0700

    added gitlab link to project

--- src/FinishedProjects.js
+++ src/FinishedProjects.js
@@ -33,6 +33,7 @@ const FinishedProjects = () => {
                             <br/>
                             The site as it sits now is in MVP state and will be eventually gain more functionality but, as of now, it is finished.
                         </p>
+                        <a href='https://gitlab.com/mean-coders/module-three-project'>cheese</a>
                     </div>
                     <div className="col-md-5">
                         <svg className="grow bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
